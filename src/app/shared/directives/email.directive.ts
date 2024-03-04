b7b8bdd03b3fa33efb554fa0dc6a094671a82e2d
Daniel Bednarek <Daniel_Bednarek@epam.com>
2024-03-04 16:52:53 +0100

    fix build issues

--- src/app/shared/directives/email.directive.ts
+++ src/app/shared/directives/email.directive.ts
@@ -2,9 +2,14 @@ import { Directive } from "@angular/core";
 import { Validator } from "@angular/forms";
 
 @Directive({
-    selector: '[emailValidator]',
-    providers: [/*Add your code here*/]
+	selector: "[emailValidator]",
+	providers: [
+		/*Add your code here*/
+	],
 })
 export class EmailValidatorDirective implements Validator {
-    // Add your code here
+	// Add your code here
+	validate(control: any): any | null {
+		return { custom: true };
+	}
 }
