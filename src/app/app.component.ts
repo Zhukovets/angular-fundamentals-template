54a21650b10e9586869da408197201cf710be0da
Daniel Bednarek <Daniel_Bednarek@epam.com>
2024-03-08 13:33:19 +0100

    add new component templates, set up header and button components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: "app-root",
+	templateUrl: "./app.component.html",
+	styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'courses-app';
+	constructor() {}
+	title = "courses-app";
+	currentButtonText = "LOGIN";
+	currentIconName: "delete" | "edit" | "" = "";
 }
--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -1,16 +1,27 @@
-import { Component } from '@angular/core';
-import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
-import { fas } from '@fortawesome/free-solid-svg-icons';
+import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { FaIconLibrary } from "@fortawesome/angular-fontawesome";
+import { faTrashCan, fas, faPencil } from "@fortawesome/free-solid-svg-icons";
 
 @Component({
-  selector: 'app-button',
-  templateUrl: './button.component.html',
-  styleUrls: ['./button.component.scss']
+	selector: "app-button",
+	templateUrl: "./button.component.html",
+	styleUrls: ["./button.component.scss"],
 })
 export class ButtonComponent {
-  constructor(library: FaIconLibrary) {
-    library.addIconPacks(fas);
-  }
+	@Input() buttonText: string | undefined;
+	@Input() iconName: string | undefined;
+	@Output() newButtonEvent = new EventEmitter<any>();
 
-  // Use the names for the inputs `buttonText` and `iconName`.
+	loginLogout(value: string) {
+		// this.newButtonEvent.emit(value)
+		console.log(value);
+	}
+	faTrashCan = faTrashCan;
+	faPencil = faPencil;
+
+	constructor(library: FaIconLibrary) {
+		library.addIconPacks(fas);
+	}
+
+	// Use the names for the inputs `buttonText` and `iconName`.
 }
