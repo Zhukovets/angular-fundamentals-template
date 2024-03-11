e2427473209b41eb2da4ae6e97b9cb0014a40e5e
Aliaksandr Zhukavets <zhukovets95@gmail.com>
2024-03-11 12:53:00 +0100

    fix: fixed build issue

--- src/app/shared/directives/email.directive.ts
+++ src/app/shared/directives/email.directive.ts
@@ -1,10 +1,9 @@
 import { Directive } from "@angular/core";
-import { Validator } from "@angular/forms";
 
 @Directive({
     selector: '[emailValidator]',
     providers: [/*Add your code here*/]
 })
-export class EmailValidatorDirective implements Validator {
+export class EmailValidatorDirective {
     // Add your code here
 }
