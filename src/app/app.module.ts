cf2ba6daf3fccfe7a3a91a8f440edaf89e8e6dd7
olewna <dajcie.suba2002@gmail.com>
2024-09-12 15:41:47 +0200

    task 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,26 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { SharedModule } from '@shared/shared.module';
-import { AppComponent } from '@app/app.component';
-import { CourseInfoComponent } from '@features/course-info/course-info.component';
-import { NotAuthorizedGuard } from '@app/auth/guards/not-authorized.guard';
-import { AuthorizedGuard } from '@app/auth/guards/authorized.guard';
-import { CoursesStoreService } from '@app/services/courses-store.service';
-import { CoursesService } from '@app/services/courses.service';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { SharedModule } from "@shared/shared.module";
+import { AppComponent } from "@app/app.component";
+import { CourseInfoComponent } from "@features/course-info/course-info.component";
+import { NotAuthorizedGuard } from "@app/auth/guards/not-authorized.guard";
+import { AuthorizedGuard } from "@app/auth/guards/authorized.guard";
+import { CoursesStoreService } from "@app/services/courses-store.service";
+import { CoursesService } from "@app/services/courses.service";
+import { CoursesComponent } from "./features/courses/courses.component";
+import { CoursesListComponent } from "./features/courses/courses-list/courses-list.component";
+import { CourseInfoModule } from "./features/course-info/course-info.module";
 
 @NgModule({
-  declarations: [AppComponent, CourseInfoComponent],
-  imports: [
-    BrowserModule,
-    SharedModule,
-    FontAwesomeModule,
+  declarations: [AppComponent, CoursesComponent, CoursesListComponent],
+  imports: [BrowserModule, SharedModule, FontAwesomeModule, CourseInfoModule],
+  providers: [
+    AuthorizedGuard,
+    NotAuthorizedGuard,
+    CoursesService,
+    CoursesStoreService,
   ],
-  providers: [AuthorizedGuard, NotAuthorizedGuard, CoursesService, CoursesStoreService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
