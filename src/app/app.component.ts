50dd3dadee772d325e304c7e6a73900fe30ebf1f
nbence96 <nemethbence1111@gmail.com>
2024-09-14 19:12:31 +0200

    added variable for testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  //creationDate for testing the components
+  creationDate = new Date();
   title = 'courses-app';
 }
