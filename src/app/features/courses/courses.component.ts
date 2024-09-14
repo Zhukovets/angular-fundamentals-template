74df8c1cd83f727d476d58bd0f2159f2944c2dff
nbence96 <nemethbence1111@gmail.com>
2024-09-14 20:24:37 +0200

    fixing typo

--- src/app/features/courses/courses.component.ts
+++ src/app/features/courses/courses.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.css']
+  styleUrls: ['./courses.component.scss']
 })
 export class CoursesComponent {
 
