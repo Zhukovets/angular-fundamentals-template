3b69c4a3b3920dd7275b548d4283115dbcd865de
Piotr <piotrjasiulewiczkontakt@gmail.com>
2024-09-20 19:58:46 +0200

    Added login and register validation

--- src/app/shared/components/login-form/login-form.component.ts
+++ src/app/shared/components/login-form/login-form.component.ts
@@ -1,12 +1,19 @@
-import { Component, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, ViewChild } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'app-login-form',
-  templateUrl: './login-form.component.html',
-  styleUrls: ['./login-form.component.scss'],
+  selector: "app-login-form",
+  templateUrl: "./login-form.component.html",
+  styleUrls: ["./login-form.component.scss"],
 })
 export class LoginFormComponent {
   @ViewChild("loginForm") public loginForm!: NgForm;
   //Use the names `email` and `password` for form controls.
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log("Form Submitted!", this.loginForm.value);
+    } else {
+      console.log("Form is invalid");
+    }
+  }
 }
