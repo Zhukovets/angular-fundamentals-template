5d3c8882d0b11c80ac289a558322dbbe7b2abe24
nbence96 <nemethbence1111@gmail.com>
2024-09-20 16:23:50 +0200

    updated login component

--- src/app/shared/components/login-form/login-form.component.ts
+++ src/app/shared/components/login-form/login-form.component.ts
@@ -7,6 +7,23 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./login-form.component.scss'],
 })
 export class LoginFormComponent {
-  @ViewChild("loginForm") public loginForm!: NgForm;
-  //Use the names `email` and `password` for form controls.
+  @ViewChild('loginForm') public loginForm!: NgForm;
+  
+  onSubmit(): void {
+    console.log(this.loginForm.submitted);
+    if (this.loginForm.valid) {
+      const email = this.loginForm.controls['email'].value;
+      const password = this.loginForm.controls['password'].value;
+      console.log('Email:', email, 'Password:', password);
+    } else {
+      console.log('Form is invalid');
+    }
+  }
+
+  triggerSubmit() {
+    const submitButton = document.querySelector('button[type="submit"]') as HTMLButtonElement;
+    if (submitButton) {
+      submitButton.click();
+    }
+  }
 }
