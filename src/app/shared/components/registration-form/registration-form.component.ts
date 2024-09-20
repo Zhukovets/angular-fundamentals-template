87bcb040a7bf04928417b1a185942fa3ef3bb146
nbence96 <nemethbence1111@gmail.com>
2024-09-20 16:24:11 +0200

    updated registration component

--- src/app/shared/components/registration-form/registration-form.component.ts
+++ src/app/shared/components/registration-form/registration-form.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registration-form',
@@ -8,5 +9,37 @@ import { FormGroup } from '@angular/forms';
 })
 export class RegistrationFormComponent {
   registrationForm!: FormGroup;
-  // Use the names `name`, `email`, `password` for the form controls.
+
+  constructor(private router: Router) {
+    this.buildForm();
+  }
+
+  buildForm(): void{
+    this.registrationForm = new FormGroup({
+      name: new FormControl('',[Validators.required]),
+      email: new FormControl('',[Validators.required]),
+      password: new FormControl('',[Validators.required,Validators.minLength(6)]),
+    });
+  }
+
+  onSubmit(): void {
+    console.log(this.registrationForm.valid);
+    if (this.registrationForm.valid) {
+      console.log(this.registrationForm.value);
+      //Database operation here
+    } else {
+      this.registrationForm.markAllAsTouched();
+    }
+  }
+
+  triggerSubmit() {
+    const submitButton = document.querySelector('button[type="submit"]') as HTMLButtonElement;
+    if (submitButton) {
+      submitButton.click();
+    }
+  }
+
+  navigateToLogin(): void {
+    this.router.navigate(['/login']);
+  }
 }
