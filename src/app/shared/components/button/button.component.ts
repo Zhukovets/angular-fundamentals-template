58905523abc0f56fa00de3e4b2aacedf93a90078
Piotr <piotrjasiulewiczkontakt@gmail.com>
2024-09-20 10:42:23 +0200

    Styling button

--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -1,6 +1,11 @@
 import { Component, Input } from "@angular/core";
 import { FaIconLibrary } from "@fortawesome/angular-fontawesome";
-import { fas } from "@fortawesome/free-solid-svg-icons";
+import { fas, IconDefinition } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCoffee,
+  faTrashCan,
+  faPencil,
+} from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: "app-button",
@@ -12,7 +17,23 @@ export class ButtonComponent {
     library.addIconPacks(fas);
   }
 
-  @Input() button_text: string = "";
+  @Input() button_text: string = "Login";
   @Input() icon_name: string = "";
+
+  iconMap: { [key: string]: any } = {
+    coffee: faCoffee,
+    trash: faTrashCan,
+    pencil: faPencil,
+  };
+
+  faCoffee = faCoffee;
+  faTrashCan = faTrashCan;
+  faPencil = faPencil;
+
+  selectedIcon: any;
+
+  ngOnChanges() {
+    this.selectedIcon = this.iconMap[this.icon_name] || null;
+  }
   // Use the names for the inputs `buttonText` and `iconName`.
 }
