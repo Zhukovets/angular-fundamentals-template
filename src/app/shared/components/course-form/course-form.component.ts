2d1d71a34acbb2f8ebb8c671d0615dc8c65cdc35
Piotr <piotrjasiulewiczkontakt@gmail.com>
2024-09-21 16:40:05 +0200

    Added reactive form validation

--- src/app/shared/components/course-form/course-form.component.ts
+++ src/app/shared/components/course-form/course-form.component.ts
@@ -1,19 +1,62 @@
-import { Component } from '@angular/core';
-import {
-  FormBuilder, FormGroup
-} from '@angular/forms';
-import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
-import { fas } from '@fortawesome/free-solid-svg-icons';
+import { Component } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { FaIconLibrary } from "@fortawesome/angular-fontawesome";
+import { fas } from "@fortawesome/free-solid-svg-icons";
 
 @Component({
-  selector: 'app-course-form',
-  templateUrl: './course-form.component.html',
-  styleUrls: ['./course-form.component.scss'],
+  selector: "app-course-form",
+  templateUrl: "./course-form.component.html",
+  styleUrls: ["./course-form.component.scss"],
 })
 export class CourseFormComponent {
-  constructor(public fb: FormBuilder, public library: FaIconLibrary) {
+  constructor(
+    public fb: FormBuilder,
+    public library: FaIconLibrary
+  ) {
     library.addIconPacks(fas);
   }
   courseForm!: FormGroup;
-  // Use the names `title`, `description`, `author`, 'authors' (for authors list), `duration` for the form controls.
+  authors: any[] = [];
+  durationText: string = "";
+  submitted = false;
+
+  ngOnInit(): void {
+    this.courseForm = this.fb.group({
+      title: ["", [Validators.required, Validators.minLength(2)]],
+      description: ["", [Validators.required, Validators.minLength(2)]],
+      author_name: ["", Validators.pattern("[a-zA-Z0-9 ]*")],
+      duration: ["", [Validators.required, Validators.min(1)]],
+    });
+
+    this.courseForm.get("duration")?.valueChanges.subscribe((value) => {
+      this.durationText = `${value} minutes`;
+    });
+  }
+
+  addAuthor() {
+    const authorName = this.courseForm.get("author_name")?.value;
+    if (
+      authorName &&
+      !this.authors.some((author) => author.name === authorName)
+    ) {
+      const authorId = Math.random().toString(36).substr(2, 9);
+      this.authors.push({ id: authorId, name: authorName });
+      this.courseForm.get("author_name")?.reset();
+    }
+  }
+
+  removeAuthor(author: string) {
+    this.authors = this.authors.filter((a) => a !== author);
+  }
+
+  onSubmit() {
+    if (this.courseForm.valid) {
+      const courseData = this.courseForm.value;
+      courseData.authors = this.authors;
+      console.log("Form submitted:", courseData);
+    } else {
+      console.log("Form is invalid");
+      this.courseForm.markAllAsTouched();
+    }
+  }
 }
