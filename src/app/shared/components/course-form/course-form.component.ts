a90059ebf780434078bb09875e9758a518764583
Piotr <piotrjasiulewiczkontakt@gmail.com>
2024-09-21 16:49:53 +0200

    Added duration validation

--- src/app/shared/components/course-form/course-form.component.ts
+++ src/app/shared/components/course-form/course-form.component.ts
@@ -59,4 +59,12 @@ export class CourseFormComponent {
       this.courseForm.markAllAsTouched();
     }
   }
+
+  getDurationFormatted(minutes: number): string {
+    const hours = Math.floor(minutes / 60);
+    const mins = minutes % 60;
+    return `${hours.toString().padStart(2, "0")}:${mins
+      .toString()
+      .padStart(2, "0")}`;
+  }
 }
