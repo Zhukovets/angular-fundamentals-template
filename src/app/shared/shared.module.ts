c42f8a6f4d4729a9ff079f65ea4bd827c41e2785
Detti <l33thomasee@gmail.com>
2024-09-22 20:54:36 +0200

    3rd task 5th commit

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,7 +1,7 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { ModalComponent } from './components/modal/modal.component';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { ModalComponent } from "./components/modal/modal.component";
 import {
   HeaderComponent,
   ButtonComponent,
@@ -10,12 +10,12 @@ import {
   CourseCardComponent,
   LoginFormComponent,
   RegistrationFormComponent,
-  CourseFormComponent
+  CourseFormComponent,
 } from "./components";
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import { DurationPipe } from './pipes/duration.pipe';
-import { CustomDatePipe } from './pipes/custom-date.pipe';
-import { EmailValidatorDirective } from '@shared/directives/email.directive';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { DurationPipe } from "./pipes/duration.pipe";
+import { CustomDatePipe } from "./pipes/custom-date.pipe";
+import { EmailValidatorDirective } from "@shared/directives/email.directive";
 
 const components = [
   HeaderComponent,
@@ -27,19 +27,14 @@ const components = [
   LoginFormComponent,
   RegistrationFormComponent,
   CourseFormComponent,
-  DurationPipe,
-  CustomDatePipe,
-  EmailValidatorDirective
+  EmailValidatorDirective,
 ];
 
+const pipes = [DurationPipe, CustomDatePipe];
+
 @NgModule({
-  declarations: [components],
-  imports: [
-    CommonModule,
-    FontAwesomeModule,
-    FormsModule,
-    ReactiveFormsModule,
-  ],
-  exports: [components]
+  declarations: [...components, ...pipes, EmailValidatorDirective],
+  imports: [CommonModule, FontAwesomeModule, FormsModule, ReactiveFormsModule],
+  exports: [...components, ...pipes],
 })
-export class SharedModule { }
+export class SharedModule {}
