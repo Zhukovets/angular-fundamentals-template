d6ef30096fd55e6797b198e666a93fc839273276
Marek-Kosmowski <kosmowski.marek@gmail.com>
2024-09-25 17:32:04 +0200

    Clear all files

--- src/app/shared/components/course-card/course-card.component.ts
+++ src/app/shared/components/course-card/course-card.component.ts
@@ -1,23 +1,8 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-course-card',
   templateUrl: './course-card.component.html',
-  styleUrls: ['./course-card.component.scss'],
+  styleUrls: ['./course-card.component.scss']
 })
-export class CourseCardComponent {
-  @Input() cardTitle!: string;
-  @Input() cardDescription!: string;
-  @Input() creationDate!: Date;
-  @Input() cardDuration!: number;
-  @Input() cardAuthors!: string[];
-
-  @Input() cardEditable: boolean = false;
-
-  // @Output() deleteRequest = new EventEmitter<Item>()
-  @Output() clickOnShow = new EventEmitter<void>();
-
-  showCourse() {
-    this.clickOnShow.emit();
-  }
-}
+export class CourseCardComponent {}
