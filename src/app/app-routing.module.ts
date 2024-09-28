87b1dfaada17cb436873c1cd5db7eb9026007798
nbence96 <nemethbence1111@gmail.com>
2024-09-28 16:27:22 +0200

    1. Implement app-routing.module.ts with lazy load modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,37 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 export const routes: Routes = [
-    /* Add your code here */
-];
+    {
+      path: 'login',
+      loadChildren: () => import('./shared/shared.module').then(m => m.SharedModule)
+    },
+    {
+      path: 'registration',
+      loadChildren: () => import('./shared/shared.module').then(m => m.SharedModule)
+    },
+    {
+        path: 'courses',
+        loadChildren: () => import('./shared/shared.module').then(m => m.SharedModule),
+        children: [
+          { path: 'add', loadChildren: () => import('./shared/shared.module').then(m => m.SharedModule) },
+          { path: ':id', loadChildren: () => import('./shared/shared.module').then(m => m.SharedModule) },
+          { path: 'edit/:id', loadChildren: () => import('./shared/shared.module').then(m => m.SharedModule) }
+        ]
+      },
+    {
+      path: '',
+      redirectTo: '/courses',
+      pathMatch: 'full'
+    },
+    {
+      path: '**',
+      redirectTo: '/courses'
+    }
+  ];
+
+  @NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule {}
