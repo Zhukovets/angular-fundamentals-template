dc2b243a1ca06f27a5417bfd1f6efae9df0abd50
nbence96 <nemethbence1111@gmail.com>
2024-10-01 22:04:24 +0200

    added interceptor

--- src/app/auth/interceptors/token.interceptor.ts
+++ src/app/auth/interceptors/token.interceptor.ts
@@ -1,7 +1,37 @@
 import { Injectable } from '@angular/core';
-import { HttpInterceptor } from '@angular/common/http';
+import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { catchError, Observable, throwError } from 'rxjs';
+import { SessionStorageService } from '../services/session-storage.service';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
-    // Add your code here
+    
+    constructor(
+        private sessionStorage: SessionStorageService,
+        private router: Router,
+        private authService: AuthService
+    ){}
+    
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        const token = this.sessionStorage.getToken();
+
+        let authReq = req;
+        if(token){
+            authReq = req.clone({
+                setHeaders: { Authorization: `Bearer ${token}` }
+            });
+        }
+
+        return next.handle(authReq).pipe(
+            catchError((error: HttpErrorResponse) => {
+                if (error.status === 401) {
+                    this.authService.logout();
+                    this.router.navigate(['/login']);
+                }
+                return throwError(error);
+            })
+        );
+    }
 }
