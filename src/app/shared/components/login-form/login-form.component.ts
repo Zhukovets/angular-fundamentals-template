7407e216e67f915e49c1b38380565c7e39d8e8b9
nbence96 <nemethbence1111@gmail.com>
2024-10-01 21:59:49 +0200

    updated login form

--- src/app/shared/components/login-form/login-form.component.ts
+++ src/app/shared/components/login-form/login-form.component.ts
@@ -1,5 +1,8 @@
 import { ChangeDetectionStrategy, Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthService } from '@app/auth/services/auth.service';
+import { ApiResponse } from '@app/models/course.model';
+import { User } from '@app/models/user.model';
 
 @Component({
   selector: 'app-login-form',
@@ -17,6 +20,8 @@ export class LoginFormComponent {
   emailControl: string = "";
   passwordControl: string = "";
 
+  constructor(private authService: AuthService){}
+
   onSubmit(): void {
     Object.keys(this.loginForm.controls).forEach(controlName => {
       const control = this.loginForm.controls[controlName];
@@ -24,10 +29,30 @@ export class LoginFormComponent {
     });
 
     if (this.loginForm.valid) {
-      console.log('Email:', this.emailControl, 'Password:', this.passwordControl);
+      const loggingInUser: User = {
+        email: this.emailControl,
+        password: this.passwordControl
+      }
+      this.login(loggingInUser);
+      this.authService.login(loggingInUser);
     } else {
       console.log('Form is invalid');
     }
   }
 
+  login(loggingInUser: User): void{
+    this.authService.login(loggingInUser).subscribe(
+      (response: ApiResponse<string>) => {
+        if(response.successful) {
+          console.log(response.result);
+          //this works, but i couldnt @Inject Window in session-storage service
+        } else {
+          console.error('failed to log in', response);
+        }
+      },
+      (error) => {
+        console.error('Error logging in', error);
+      }
+    );
+  }
 }
