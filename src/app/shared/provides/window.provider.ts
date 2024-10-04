050724b5230a89efc4d93d5ffe53838504171876
Piotr <piotrjasiulewiczkontakt@gmail.com>
2024-10-04 12:45:13 +0200

    Poprawa window serwisu

--- src/app/shared/provides/window.provider.ts
+++ src/app/shared/provides/window.provider.ts
@@ -1,7 +1,5 @@
 import { Injectable } from "@angular/core";
 
-// This interface is optional, showing how you can add strong typings for custom globals.
-// Just use "Window" as the type if you don't have custom global stuff
 export interface ICustomWindow extends Window {
   __custom_global_stuff: string;
 }
@@ -10,7 +8,9 @@ function getWindow(): any {
   return window;
 }
 
-@Injectable()
+@Injectable({
+  providedIn: "root",
+})
 export class WindowRefService {
   get nativeWindow(): ICustomWindow {
     return getWindow();
