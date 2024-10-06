386c972deeffacb3b24f50cd78c4bfb7781b7925
Detti <l33thomasee@gmail.com>
2024-10-06 18:43:47 +0200

    Fix CustomDatePipe import

--- src/app/courses/courses-list/courses-list.component.ts
+++ src/app/courses/courses-list/courses-list.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from "@angular/core";
 import { Course } from "@app/models/course.model";
 import { CoursesStateFacade } from "@app/store/courses/courses.facade";
 import { Observable } from "rxjs";
+import { CustomDatePipe } from "@app/shared/pipes/custom-date.pipe";
 
 @Component({
   selector: "app-courses-list",
@@ -10,12 +11,14 @@ import { Observable } from "rxjs";
 })
 export class CoursesListComponent {
   courses$: Observable<Course[] | null>;
+  creationDate: CustomDatePipe;
 
   @Input() courseName: string = "";
   @Input() isEditable: boolean = false;
 
   constructor(private coursesFacade: CoursesStateFacade) {
     this.courses$ = this.coursesFacade.allCourses$;
+    this.creationDate = new CustomDatePipe();
     this.getCourses();
   }
 
