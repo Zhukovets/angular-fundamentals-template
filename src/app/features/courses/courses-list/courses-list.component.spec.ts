eefbb4253ef8488a9246d2393634b8dfe827db76
Akos Donko <donko.akos@gmail.com>
2024-10-06 11:05:50 +0200

    5th task 2nd commit, store error fix try

--- src/app/features/courses/courses-list/courses-list.component.spec.ts
+++ src/app/features/courses/courses-list/courses-list.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { CoursesListComponent } from './courses-list.component';
+import { StoreModule } from '@ngrx/store';
 
 describe('CoursesListComponent', () => {
   let component: CoursesListComponent;
@@ -8,7 +8,10 @@ describe('CoursesListComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CoursesListComponent ]
+      declarations: [ CoursesListComponent ],
+      imports: [
+        StoreModule.forRoot({})
+      ]
     })
     .compileComponents();
 
